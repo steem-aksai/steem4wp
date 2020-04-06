62f3401dd88d4e4496fd59a05d6e5ff9a773e33a
think-in-universe <mstr.raphael@gmail.com>
2020-04-06 17:12:29 +0800

    add user configuration

--- includes/hooks.php
+++ includes/hooks.php
@@ -10,4 +10,57 @@ add_filter( 'user_contactmethods',function($userInfo) {
 	return $userInfo;
 });
 
+add_action( 'personal_options_update', 'update_steem_user_settings' );
+add_action( 'edit_user_profile_update', 'update_steem_user_settings' );
+
+function update_steem_user_settings( $user_id ) {
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+	update_user_meta( $user_id, 'user_steem_id', $_POST['user_steem_id'] );
+	update_user_meta( $user_id, 'user_steem_posting_key', $_POST['user_steem_posting_key'] );
+	update_user_meta( $user_id, 'user_steem_default_tags', $_POST['user_steem_default_tags'] );
+	update_user_meta( $user_id, 'user_steem_footer', $_POST['user_steem_footer'] );
+}
+
+add_action( 'show_user_profile', 'get_steem_user_settings' );
+add_action( 'edit_user_profile', 'get_steem_user_settings' );
+
+function get_default_footer() {
+	$default_footer = get_the_author_meta( 'user_steem_footer', $user->ID );
+	if (empty($default_footer)) {
+		$default_footer = "<br /><center><hr/><em>本文使用 <a href='https://github.com/steem-aksai/steem4wp'>Steem4WP</a> 发布；原文来自 : [%original_link%] </em><hr/></center>";
+	}
+	return $default_footer;
+}
+
+function get_steem_user_settings( $user ) { ?>
+<h2>Steem for Wordpress</h2>
+<table class="form-table">
+    <tr>
+        <th><label for="user_steem_id">Steem ID</label></th>
+        <td>
+            <input type="text" name="user_steem_id" id="user_steem_id" class="regular-text" value="<?php echo htmlspecialchars(get_the_author_meta( 'user_steem_id', $user->ID )); ?>"/>
+        </td>
+    </tr>
+    <tr>
+        <th><label for="user_steem_posting_key">发帖秘钥</label></th>
+        <td>
+            <input type="password" name="user_steem_posting_key" id="user_steem_posting_key" class="regular-text" value="<?php echo htmlspecialchars(get_the_author_meta( 'user_steem_posting_key', $user->ID )); ?>"/>
+        </td>
+    </tr>
+    <tr>
+        <th><label for="user_steem_default_tags">默认标签</label></th>
+        <td>
+            <input type="text" name="user_steem_default_tags" id="user_steem_default_tags" class="regular-text" value="<?php echo htmlspecialchars(get_the_author_meta( 'user_steem_default_tags', $user->ID )); ?>"/>
+        </td>
+    </tr>
+    <tr>
+        <th><label for="user_steem_footer">文章脚注</label></th>
+        <td>
+          <textarea name="user_steem_footer" id="user_steem_footer" rows="5" columns="30"> <?php echo htmlspecialchars(get_default_footer()); ?> </textarea>
+        </td>
+    </tr>
+</table>
+<?php }
+
 ?>
